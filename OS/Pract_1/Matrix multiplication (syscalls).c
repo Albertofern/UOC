4038da54d70652dfbeb4414c5b5e872b3a573ea4
David Doblas Jiménez <daviddoji@protonmail.com>
2017-01-05 12:05:54 +0100

    Update Matrix multiplication (syscalls).c

--- OS/Pract_1/Matrix multiplication (syscalls).c
+++ OS/Pract_1/Matrix multiplication (syscalls).c
@@ -3,10 +3,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/* Size of the matrix*/
 #define N 1000
 
-/* Matrix */ 
-long long int A[N][N],B[N][N],R[N][N];
+/* Matrix variables*/ 
+long long int A[N][N], B[N][N], R[N][N];
 	
 int main(int argc, char *argv[])
 {
@@ -15,19 +16,19 @@ int main(int argc, char *argv[])
 
   /* Matrix initialization */
   for(y=0; y<N; y++) 
-		for(x=0; x<N; x++)
-		{
-		  A[y][x]=x;
-		  B[y][x]=y;
-		  R[y][x]=0;	
-		}
+    for(x=0; x<N; x++)
+      {
+	A[y][x] = x;
+	B[y][x] = y;
+	R[y][x] = 0;	
+      }
 		
   /* Matrix multiplication */
   for(y=0; y<N; y++)
     for(z=0; z<N; z++) 
       for(x=0; x<N; x++) 
       {
-        R[y][x]+= A[y][z] * B[z][x];	
+        R[y][x] += A[y][z] * B[z][x];	
       }
 
   /* Print results */
