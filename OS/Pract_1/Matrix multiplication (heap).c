abb92c8e083566e21e3f134e30c11d77cfd02149
David Doblas Jiménez <daviddoji@protonmail.com>
2017-01-05 11:06:05 +0100

    Update Matrix multiplication (heap).c

--- OS/Pract_1/Matrix multiplication (heap).c
+++ OS/Pract_1/Matrix multiplication (heap).c
@@ -6,27 +6,27 @@
 	
 int main(int argc, char *argv[])
 {
-	// Matrix 
-	long long int A[N][N],B[N][N],R[N][N];
-	int x,y,z;
-	char str[100];
+  // Matrix 
+  long long int A[N][N],B[N][N],R[N][N];
+  int x,y,z;
+  char str[100];
 
-	/* Matrix inicialization */
-	for(y=0;y<N;y++) 
-		for(x=0;x<N;x++)
-		{
-			A[y][x]=x;
-			B[y][x]=y;
-			R[y][x]=0;	
-		}
+  /* Matrix inicialization */
+  for(y=0;y<N;y++) 
+    for(x=0;x<N;x++)
+    {
+      A[y][x]=x;
+      B[y][x]=y;
+      R[y][x]=0;	
+    }
 		
-	/* Matrix multiplication */
-	for(y=0;y<N;y++)
-		for(z=0;z<N;z++) 
-			for(x=0;x<N;x++) 
-			{
-				R[y][x]+= A[y][z] * B[z][x];	
-			}       
+  /* Matrix multiplication */
+  for(y=0;y<N;y++)
+    for(z=0;z<N;z++) 
+      for(x=0;x<N;x++) 
+      {
+        R[y][x]+= A[y][z] * B[z][x];	
+      }       
 
-	exit(0);
+   exit(0);
 }
