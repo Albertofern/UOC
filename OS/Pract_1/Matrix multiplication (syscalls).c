22f4e0d8d59c294666e4cf403962b30ada9e6191
David Doblas Jiménez <daviddoji@protonmail.com>
2017-01-05 11:48:32 +0100

    Update Matrix multiplication (syscalls).c

--- OS/Pract_1/Matrix multiplication (syscalls).c
+++ OS/Pract_1/Matrix multiplication (syscalls).c
@@ -18,30 +18,30 @@ int main(int argc, char *argv[])
 		for(x=0; x<N; x++)
 		{
 		  A[y][x]=x;
-			B[y][x]=y;
-			R[y][x]=0;	
+		  B[y][x]=y;
+		  R[y][x]=0;	
 		}
 		
-	/* Matrix multiplication */
-	for(y=0; y<N; y++)
-		for(z=0; z<N; z++) 
-			for(x=0; x<N; x++) 
-			{
-				R[y][x]+= A[y][z] * B[z][x];	
-			}
+  /* Matrix multiplication */
+  for(y=0; y<N; y++)
+    for(z=0; z<N; z++) 
+      for(x=0; x<N; x++) 
+      {
+        R[y][x]+= A[y][z] * B[z][x];	
+      }
 
-	/* Print results */
-    	for(y=0; y<N; y++)
-    	{
-    	  const char *space = "";
-    	  for(x=0; x<N; x++)
-    	  {
-    	    sprintf(str, "%s%lli", space, R[y][x]);
-    	    write(1, str, strlen(str));
-    	    space = " ";
-    	  }
-    	  write(1, "\n", 1);
-    	}     
+  /* Print results */
+  for(y=0; y<N; y++)
+  {
+    const char *space = "";
+    for(x=0; x<N; x++)
+    {
+      sprintf(str, "%s%lli", space, R[y][x]);
+      write(1, str, strlen(str));
+      space = " ";
+    }
+    write(1, "\n", 1);
+  }     
       
-	exit(0);
+  exit(0);
 }
