6b4d6ba69011b0f0dc2dc51c7287e5d927680f0d
David Doblas Jiménez <daviddoji@protonmail.com>
2017-01-05 11:03:13 +0100

    Create Matrix Multiplication (heap).c

---
+++ OS/Pract_1/Matrix multiplication (heap).c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define N 1000
+	
+int main(int argc, char *argv[])
+{
+	// Matrix 
+	long long int A[N][N],B[N][N],R[N][N];
+	int x,y,z;
+	char str[100];
+
+	/* Matrix inicialization */
+	for(y=0;y<N;y++) 
+		for(x=0;x<N;x++)
+		{
+			A[y][x]=x;
+			B[y][x]=y;
+			R[y][x]=0;	
+		}
+		
+	/* Matrix multiplication */
+	for(y=0;y<N;y++)
+		for(z=0;z<N;z++) 
+			for(x=0;x<N;x++) 
+			{
+				R[y][x]+= A[y][z] * B[z][x];	
+			}       
+
+	exit(0);
+}
