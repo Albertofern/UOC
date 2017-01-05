2846974b3cdbcd48d2ae0202ed30e7b7fe98c531
David Doblas Jiménez <daviddoji@protonmail.com>
2017-01-05 11:11:06 +0100

    Update Matrix multiplication (heap).c

--- OS/Pract_1/Matrix multiplication (heap).c
+++ OS/Pract_1/Matrix multiplication (heap).c
@@ -2,18 +2,19 @@
 #include <string.h>
 #include <stdlib.h>
 
+/* Size of the matrix */
 #define N 1000
 	
 int main(int argc, char *argv[])
 {
-  // Matrix 
+  /* Matrix variables */ 
   long long int A[N][N],B[N][N],R[N][N];
   int x,y,z;
   char str[100];
 
-  /* Matrix inicialization */
-  for(y=0;y<N;y++) 
-    for(x=0;x<N;x++)
+  /* Matrix initialization */
+  for(y=0; y<N; y++) 
+    for(x=0; x<N; x++)
     {
       A[y][x]=x;
       B[y][x]=y;
@@ -21,9 +22,9 @@ int main(int argc, char *argv[])
     }
 		
   /* Matrix multiplication */
-  for(y=0;y<N;y++)
-    for(z=0;z<N;z++) 
-      for(x=0;x<N;x++) 
+  for(y=0; y<N; y++)
+    for(z=0; z<N; z++) 
+      for(x=0; x<N; x++) 
       {
         R[y][x]+= A[y][z] * B[z][x];	
       }       
