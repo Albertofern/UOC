2630e2b012fe5aec17606e6cc2766dfcebb1e377
David Doblas Jiménez <daviddoji@protonmail.com>
2017-01-15 19:35:23 +0100

    Update Factory.java

--- DOOP/PEC3/Ex2/src/Factory.java
+++ DOOP/PEC3/Ex2/src/Factory.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
  * @author POO teaching staff
  * @version 1.0
  * @since Autumn 2016
+ * @modified David Doblas Jiménez
  */
 
 public class Factory{
@@ -39,10 +40,10 @@ public class Factory{
     * Constructor method
     */
    public Factory() {
-      this.materials = new ArrayList<Material>();
-      this.colors = new ArrayList<Color>();
-      this.furniture = new ArrayList<Furniture>();
-      this.orders = new ArrayList<Order>();
+		 this.materials = new ArrayList<Material>();
+     this.colors = new ArrayList<Color>();
+     this.furniture = new ArrayList<Furniture>();
+     this.orders = new ArrayList<Order>();
    }
 
    /**
@@ -55,7 +56,7 @@ public class Factory{
 
   /**
    * This method adds an element to the list of colors
-   * @param m
+   * @param c
    */
  public void add(Color c) {
 	 this.colors.add(c);
@@ -63,7 +64,7 @@ public class Factory{
 
    /**
     * This method adds an element to the list of furniture
-    * @param p
+    * @param f
     */
 	public void add(Furniture f) {
 		this.furniture.add(f);
@@ -78,96 +79,85 @@ public class Factory{
 	}
 
    /**
-    * This method generates and returns the information of
-    * every element in the materials list of the factory, in
+    * This method generates and returns the information of every element in the materials list of the factory, in
     * the order they were introduced.
     */
    public String listOfMaterials() {
-      StringBuilder sb = new StringBuilder();
-      for (Iterator<Material> it = materials.iterator(); it.hasNext();) {
-          Material m = (Material) it.next();
-          sb.append(m.toString());
-       }
-      return sb.toString();
+		 StringBuilder sb = new StringBuilder();
+     for (Iterator<Material> it = materials.iterator(); it.hasNext();) {
+			 Material m = (Material) it.next();
+       sb.append(m.toString());
+     }
+     return sb.toString();
    }
 
    /**
-    * This method generates and returns the information of
-    * every element in the colors list of the factory, in
+    * This method generates and returns the information of every element in the colors list of the factory, in
     * the order they were introduced.
     */
    public String listOfColors() {
-      StringBuilder sb = new StringBuilder();
-      for (Iterator<Color> it = colors.iterator(); it.hasNext();){
-    	  Color c = (Color) it.next();
-    	  sb.append(c.toString() + NL);
-      }
-      return sb.toString();
+		 StringBuilder sb = new StringBuilder();
+     for (Iterator<Color> it = colors.iterator(); it.hasNext();){
+			 Color c = (Color) it.next();
+    	 sb.append(c.toString() + NL);
+     }
+     return sb.toString();
    }
 
    /**
-    * This method generates and returns the information of
-    * every element in the products list of the factory, in
+    * This method generates and returns the information of every element in the products list of the factory, in
     * the order they were introduced.
     */
    public String listOfFurniture() {
-      StringBuilder sb = new StringBuilder();
-      for (Iterator<Furniture> it = furniture.iterator(); it.hasNext();){
-    	  Furniture f = (Furniture) it.next();
-    	  sb.append(f.toString() + NL);
-      }
-      return sb.toString();
+		 StringBuilder sb = new StringBuilder();
+     for (Iterator<Furniture> it = furniture.iterator(); it.hasNext();){
+			 Furniture f = (Furniture) it.next();
+    	 sb.append(f.toString() + NL);
+     }
+     return sb.toString();
    }
 
    /**
-    * This method generates and returns the information of
-    * every element in the orders list of the factory, in
+    * This method generates and returns the information of every element in the orders list of the factory, in
     * the order they were introduced.
     */
    public String listOfOrders() {
-      StringBuilder sb = new StringBuilder();
-      for (Iterator<Order> it = orders.iterator(); it.hasNext();){
-    	  Order o = (Order) it.next();
-    	  sb.append(o.toString() + NL);
-      }
-      return sb.toString();
+		 StringBuilder sb = new StringBuilder();
+     for (Iterator<Order> it = orders.iterator(); it.hasNext();){
+			 Order o = (Order) it.next();
+    	 sb.append(o.toString() + NL);
+     }
+     return sb.toString();
    }
 
    /**
-    * This method generates and returns the information of
-    * those elements in the furniture list of the factory,
-    * whose price is comprised between a minimum and a maximum
-    * values (both included).
+    * This method generates and returns the information of those elements in the furniture list of the factory,
+    * whose price is comprised between a minimum and a maximum values (both included).
     * @param min
     *             minimum price
     * @param max
     *             maximum price
     */
    public String listFurnitureInPriceRange(double min, double max) {
-      StringBuilder sb = new StringBuilder();
-      for (Iterator<Furniture> it = furniture.iterator(); it.hasNext();) {
-          Furniture f = (Furniture) it.next();
-          if ((f.price() >= min) && (f.price() <= max))
-            sb.append(f.toString() + NL);
-       }
-      return sb.toString();
+		 StringBuilder sb = new StringBuilder();
+     for (Iterator<Furniture> it = furniture.iterator(); it.hasNext();) {
+			 Furniture f = (Furniture) it.next();
+       if ((f.price() >= min) && (f.price() <= max))
+				 sb.append(f.toString() + NL);
+     }
+     return sb.toString();
    }
 
    /**
-    * This method removes an Order object from the list orders.
-    * To do its task, this method has to use the appropriate
-    * method of ArrayList class.
-    * And to do that, it is necessary to redefine the equals
-    * method on class Order, to define whether two Order
-    * objects are equals (see description of equals method in
-    * class Order)
+    * This method removes an Order object from the list orders by using the appropriate method of ArrayList class.
+    * To do that, it is necessary to redefine the equals method on class Order, to define whether two Order
+    * objects are equals (see description of equals method in class Order)
     */
    public boolean removeOrder(Order order) {
 	   for(Iterator it = this.orders.iterator(); it.hasNext();) {
 		   Order myOrder = (Order)it.next();
-		   if(myOrder.equals(order)) {
+		   if(myOrder.equals(order))
 			   return this.orders.remove(order);
-		   }
 	   }
 	   return false;
    }
