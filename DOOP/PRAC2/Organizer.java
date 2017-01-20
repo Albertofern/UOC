5c65733dca89e27722966fc4f0802965e980042a
David Doblas Jiménez <daviddoji@protonmail.com>
2017-01-20 18:09:46 +0100

    Update Organizer.java

--- DOOP/PRAC2/Organizer.java
+++ DOOP/PRAC2/Organizer.java
@@ -9,7 +9,12 @@ public class Organizer extends User {
     public Organizer(User user) {
         super(user);
         // Initialize the list of competitions
-        competitions = new ArrayList<Competition>();                
+        competitions = new ArrayList<Competition>();
+        
+        /**
+         * PR2 Ex 2.2: When creating a new organizer, we need to update the competitions
+        */
+        updateCompetitions();
     }
 
     public void removeSubmission(Submission submission) {
@@ -21,19 +26,57 @@ public class Organizer extends User {
         return false;
     }
 
-    public Competition newCompetition(String title, float target) {        
-        return null;
+    public Competition newCompetition(String title, float target) {
+        /**
+         * PR2 Ex 2.1: Create a new competition
+        */
+        Competition newComp = new Competition(this.getPlatform(), this, title, target);
+        
+        // Register the competition to the platform
+        this.getPlatform().registerCompetition(newComp);
+             
+        // Add to the list of competitions of the user
+        this.competitions.add(newComp);
+        
+        return newComp;
     }
 
     public void closeCompetition(Competition competition) {
+        /**
+         * PR2 Ex 2.2: 
+        */ 
+        competition.close();
+        
+        /**
+         * PR2 Ex 2.3: Update competitions 
+        */ 
+        // When closing a competition, we need to update the competitions
+        updateCompetitions();
         
     }
     
-    public List<Competition> getCompetitions() {        
+    public List<Competition> getCompetitions() {
+        /**
+         * PR2 Ex 2.1: Required by test
+        */
         return this.competitions;
     }
     
     private void updateCompetitions() {
+        /**
+         * PR2 Ex 2.3: Update the list of competitions
+        */
+        competitions.clear();
         
+        // Get the list of competitions from the platform
+        List<Competition> currentCompetitions = this.getPlatform().getOpenCompetitions();        
+        
+        // Add the "open" competitions that belong to "this" owner
+        for (Iterator<Competition> it = currentCompetitions.iterator(); it.hasNext(); ) {
+            Competition c = it.next();
+            if(this.equals(c.getOwner())) {
+                this.competitions.add(c);
+            }
+        }   
     }
 }
