5029f567b6238cf3488e1dfac5272089d56f3272
David Doblas Jiménez <daviddoji@protonmail.com>
2017-01-20 18:45:55 +0100

    Update SubmissionStatus.java

--- DOOP/PRAC2/SubmissionStatus.java
+++ DOOP/PRAC2/SubmissionStatus.java
@@ -1,5 +1,14 @@
 package edu.uoc.dpoo;
 
+/**
+ * This class represents a message
+ *
+ * @author POO teaching staff
+ * @version 1.0
+ * @since Autumn 2016
+ * @modified David Doblas Jiménez
+ */
+
 public enum SubmissionStatus {
     PENDING,
     DONE,
