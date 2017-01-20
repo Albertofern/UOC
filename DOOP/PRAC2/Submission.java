fb1438a354d999bf6d824b167f324ae0304f24b4
David Doblas Jiménez <daviddoji@protonmail.com>
2017-01-20 18:43:15 +0100

    Update Submission.java

--- DOOP/PRAC2/Submission.java
+++ DOOP/PRAC2/Submission.java
@@ -2,7 +2,16 @@ package edu.uoc.dpoo;
 
 import java.util.Date;
 
-public class Submission {
+/**
+ * This class represents a message
+ *
+ * @author POO teaching staff
+ * @version 1.0
+ * @since Autumn 2016
+ * @modified David Doblas Jiménez
+ */
+
+public class Submission implements Comparable<Submission> {
     private Integer id;
     private Participant participant;
     private Competition competition;    
@@ -10,28 +19,78 @@ public class Submission {
     private Date submittedAt;
     private float prediction;
     private float error;
+    
+    /**
+     * PR2 Ex 3.1: Class attribute to manage new IDs
+    */
+    private static Integer lastID = 0;
       
     public Submission(Participant participant, Competition competition, float prediction) {
-       
+        /**
+         * PR2 Ex 3.1: Create a submission
+        */
+        this.participant = participant;
+        this.competition = competition;
+        this.status = SubmissionStatus.PENDING;
+        this.submittedAt = new Date();
+        this.prediction = prediction;
+        this.id=Submission.lastID + 1;
+        
+        Submission.lastID = this.id;
     }
     
-    public SubmissionStatus getStatus() {       
+    public SubmissionStatus getStatus() {
+        /**
+         * PR2 Ex 3.1: Required by tests
+        */
         return status;
     }
     
-    public float getError() {        
+    public float getError() {
+        /**
+         * PR2 Ex 3.2: Required by tests
+        */
         return this.error;
     }
     
-    public void setError(float error) {        
-        this.error = error;        
+    public void setError(float error) {
+        /**
+         * PR2 Ex 3.2: Set error value
+        */
+        this.error = error;
+        this.status = SubmissionStatus.DONE;
     }
     
-    public float getPrediction() {        
+    public float getPrediction() {
+        /**
+         * PR2 Ex 3.1: Required to calculate the error value
+        */
         return prediction;
     }
     
-    public Participant getParticipant() {        
+    public Participant getParticipant() {
+        /**
+         * PR2 Ex 3.4: Required to obtain the owner of the submission
+        */
         return this.participant;
+    }
+    
+    /**
+     * PR2 Ex 3.3: Implement compare to method
+    */
+    public int compareTo(Submission submission) {
+        int retVal;
+        if(this.status != submission.status) {
+            if(this.status == SubmissionStatus.DONE) {                
+                retVal = 1;
+            } 
+            else {
+                retVal=0;
+            }
+        } 
+        else {
+            retVal = new Float(this.error).compareTo(submission.getError());
+        }
+        return retVal;
     }
 }
