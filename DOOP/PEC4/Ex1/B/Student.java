2df46f5896aca3fdc867413417506c8a0853c7d5
David Doblas Jiménez <daviddoji@protonmail.com>
2017-01-27 19:34:15 +0100

    Update Student.java

--- DOOP/PEC4/Ex1/B/Student.java
+++ DOOP/PEC4/Ex1/B/Student.java
@@ -1,4 +1,3 @@
-
 /**
  * This class represents a student
  *
@@ -6,65 +5,66 @@
  * @version 1.0
  * @since Autumn 2016
  */
+
 public class Student {
 	
-	// The new line separator:
-	private final String NL = System.getProperty("line.separator");
-	
-	private String name;
-	private int roll_no;
-	private int mark1;
-	private int mark2;
+  // The new line separator:
+  private final String NL = System.getProperty("line.separator");
 
-   /**
-    * Constructor
-    *
-    * @param n the student name
-    * @param r the student roll number
-    * @param m1 mark of subject 1
-    * @param m2 mark of subject 2
-    */
-	public Student (String n, int r, int m1, int m2) {
-		this.name = n;
-		this.roll_no = r;
-		this.mark1 = m1;
-		this.mark2 = m2;
-	}
+  private String name;
+  private int roll_no;
+  private int mark1;
+  private int mark2;
 
-	/**
-	 * GETTERS/SETTERS
-	 */
-	public String getName () {
-		return this.name;
-	}
-	
-	
-	public int getRollNo () {
-		return this.roll_no;
-	}
-	
-	public int getMark1 () {
-		return this.mark1;
-	}
-	
-	public int getMark2 () {
-		return this.mark2;
-	}
-	
+ /**
+  * Constructor
+  *
+  * @param n the student name
+  * @param r the student roll number
+  * @param m1 mark of subject 1
+  * @param m2 mark of subject 2
+  */
 
-	public void setRollNo(int roll_no) {
-		this.roll_no = roll_no;
-	}
+  public Student (String n, int r, int m1, int m2) {
+    this.name = n;
+    this.roll_no = r;
+    this.mark1 = m1;
+    this.mark2 = m2;
+  }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+  /**
+   * GETTERS/SETTERS
+   */
+  
+  public String getName () {
+    return this.name;
+  }
+	
+  public int getRollNo () {
+    return this.roll_no;
+  }
+	
+  public int getMark1 () {
+    return this.mark1;
+  }
+	
+  public int getMark2 () {
+    return this.mark2;
+  }
+	
+  public void setRollNo(int roll_no) {
+    this.roll_no = roll_no;
+  }
 
-	public void setMark1(int mark1) {
-		this.mark1 = mark1;
-	}
+  public void setName(String name) {
+    this.name = name;
+  }
 
-	public void setMark2(int mark2) {
-		this.mark2 = mark2;
-	}
+  public void setMark1(int mark1) {
+    this.mark1 = mark1;
+  }
+
+  public void setMark2(int mark2) {
+    this.mark2 = mark2;
+  }
 }
