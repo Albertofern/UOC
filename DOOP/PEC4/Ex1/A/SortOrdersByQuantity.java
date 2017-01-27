e43b8790669fcd7ab67d1306e51228d3ab1bd090
David Doblas Jiménez <daviddoji@protonmail.com>
2017-01-27 19:43:09 +0100

    Update SortOrdersByQuantity.java

--- DOOP/PEC4/Ex1/A/SortOrdersByQuantity.java
+++ DOOP/PEC4/Ex1/A/SortOrdersByQuantity.java
@@ -1,3 +1,4 @@
+
 import java.util.Comparator;
 
 /**
@@ -8,12 +9,12 @@ import java.util.Comparator;
 
 public class SortOrdersByQuantity implements Comparator<Order> {
 	
-	/**
-	 * This method compares two objects based on the quantity
-	 * @param o1, o2 orders to be compared
-	 * @return Returns a <0, 0 or >0 if the first argument is <, =, or > than the second
-	 */
-	public int compare(Order o1, Order o2) {
-		return Integer.compare(o1.getQuantity(),o2.getQuantity());
-	}
+  /**
+   * This method compares two objects based on the quantity
+   * @param o1, o2 orders to be compared
+   * @return Returns a <0, 0 or >0 if the first argument is <, =, or > than the second
+   */
+  public int compare(Order o1, Order o2) {
+    return Integer.compare(o1.getQuantity(),o2.getQuantity());
+  }
 }
