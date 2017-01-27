43eef0b56cd996e804ace618856f95549ca817f2
David Doblas Jiménez <daviddoji@protonmail.com>
2017-01-27 19:26:35 +0100

    Update Main.java

--- DOOP/PEC4/Ex1/B/Main.java
+++ DOOP/PEC4/Ex1/B/Main.java
@@ -1,5 +1,3 @@
-
-
 import java.lang.*;
 import java.io.*;
 
@@ -13,16 +11,18 @@ import java.io.*;
  */
 
 class Main {
-    /**
-     * main method
-     * @param args arguments
-     */
-	public static void main(String args[]) {
-		//Create a result
-		Result R = new Result("Ra.one",12,93,84);
-		//display a result
-		R.display();
-		//calculate a result
-		R.percent_cal();
-	}
+  /**
+   * main method
+   * @param args arguments
+   */
+  public static void main(String args[]) {
+    /* Create a result 
+    Result R = new Result("Ra.one",12,93,84);
+    
+    /* display a result
+    R.display();
+
+    /* calculate a result
+    R.percent_cal();
+  }
 }
