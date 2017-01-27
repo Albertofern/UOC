dc7b5c7afcab070ca301b240b3ef5d5d21b65e9c
David Doblas Jiménez <daviddoji@protonmail.com>
2017-01-27 19:42:12 +0100

    Update SortOrdersByDate.java

--- DOOP/PEC4/Ex1/A/SortOrdersByDate.java
+++ DOOP/PEC4/Ex1/A/SortOrdersByDate.java
@@ -1,3 +1,4 @@
+
 import java.util.Comparator;
 
 /**
@@ -8,12 +9,13 @@ import java.util.Comparator;
 
 public class SortOrdersByDate implements Comparator<Order> {
 	
-	/**
-	 * This method compares two objects based on the date 
-	 * @param o1, o2 orders to be compared
-	 * @return Returns a <0, 0 or >0 if the first argument is <, =, or > than the second
-	 */
-	public int compare(Order o1, Order o2) {
-		return o1.getDate().compareTo(o2.getDate());
-	}
+  /**
+   * This method compares two objects based on the date 
+   * @param o1, o2 orders to be compared
+   * @return Returns a <0, 0 or >0 if the first argument is <, =, or > than the second
+   */
+	
+  public int compare(Order o1, Order o2) {
+    return o1.getDate().compareTo(o2.getDate());
+  }
 }
