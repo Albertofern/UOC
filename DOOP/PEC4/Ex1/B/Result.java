f78c2f2a1acad4438f742865afd92b102f6073e7
David Doblas Jiménez <daviddoji@protonmail.com>
2017-01-27 19:31:07 +0100

    Update Result.java

--- DOOP/PEC4/Ex1/B/Result.java
+++ DOOP/PEC4/Ex1/B/Result.java
@@ -7,41 +7,42 @@
  * @since Autumn 2016
  */
 public class Result extends Student implements IExam{
-	
-	// The new line separator:
-	private final String NL = System.getProperty("line.separator");
-	
-   /**
-    * Constructor
-    *
-    * @param n the student name
-    * @param r the student roll number
-    * @param m1 mark of subject 1
-    * @param m2 mark of subject 2
-    */
-	public Result(String n, int r, int m1, int m2){
-		/**
-		 * get the attributes from the parent class
-		 */
-		super(n, r, m1, m2);
-	}
 
-	/**
-	 * Returns info about a student 
-	 */
-	public void display() {
-	    StringBuilder sb = new StringBuilder();
-	    sb.append("Name of Student: " + this.getName() + NL);
-	    sb.append("Roll No. of Student: " + this.getRollNo() + NL);
-	    sb.append("Marks of Subject 1: " + this.getMark1() + NL);
-	    sb.append("Marks of Subject 2: " + this.getMark2());
-	    System.out.println(sb.toString());
-	}
+  // The new line separator:
+  private final String NL = System.getProperty("line.separator");
+
+  /**
+   * Constructor
+   *
+   * @param n the student name
+   * @param r the student roll number
+   * @param m1 mark of subject 1
+   * @param m2 mark of subject 2
+   */
+
+  public Result(String n, int r, int m1, int m2){
+
+  /**
+   * get the attributes from the parent class
+   */
+  super(n, r, m1, m2);
+  }
+
+  /**
+   * Returns info about a student 
+   */
+  public void display() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("Name of Student: " + this.getName() + NL);
+    sb.append("Roll No. of Student: " + this.getRollNo() + NL);
+    sb.append("Marks of Subject 1: " + this.getMark1() + NL);
+    sb.append("Marks of Subject 2: " + this.getMark2());
+    System.out.println(sb.toString());
+  }
 	
-	
-	public void percent_cal() {
-		StringBuilder sb = new StringBuilder();
-	  sb.append("Percentage: " + (((float)this.getMark1() + this.getMark2()) * 100/200) + "%");
-	  System.out.println(sb.toString());
-	}
+  public void percent_cal() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("Percentage: " + (((float)this.getMark1() + this.getMark2()) * 100/200) + "%");
+    System.out.println(sb.toString());
+  }
 }
